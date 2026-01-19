e2f954cb44b21facbb3d2b0b77daa94eb9bba9b9
voomdoon <nilreb24-github20141105@yahoo.de>
2026-01-19 20:26:40 +0100

    config: add module

--- apply-to-all.sh
+++ apply-to-all.sh
@@ -1,5 +1,7 @@
 ./apply.sh /mnt/c/workspaces/vd/public-java/vd-parent
 
+./apply.sh /mnt/c/workspaces/vd/public-logging/vd-logging
+
 ./apply.sh /mnt/c/workspaces/vd/public-testing/vd-testing-file
 
 ./apply.sh /mnt/c/workspaces/vd/public-util/vd-util-parent
