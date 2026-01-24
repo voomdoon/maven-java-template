4c24e1b768b8c799780539099b76367b5f292e0e
voomdoon <nilreb24-github20141105@yahoo.de>
2026-01-24 10:20:56 +0100

    feat: apply-to-released.bat

---
+++ apply-to-released.bat
@@ -0,0 +1,7 @@
+CALL apply.bat C:\workspaces\vd\public-java\vd-parent
+
+CALL apply.bat C:\workspaces\vd\public-logging\vd-logging
+
+CALL apply.bat C:\workspaces\vd\public-testing\vd-testing-file
+
+CALL apply.bat C:\workspaces\vd\public-util\vd-util-parent
