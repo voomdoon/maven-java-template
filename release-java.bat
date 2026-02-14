9e29cd71fc7213f65466145c214ece8b7182e1fb
voomdoon <9573863+voomdoon@users.noreply.github.com>
2026-02-14 17:27:11 +0100

    feat: add more GIT checks

--- release-java.bat
+++ release-java.bat
@@ -1,13 +1,26 @@
-::@ECHO OFF
+:: Release script for Java projects using Maven.
+
+@ECHO OFF
 
 setlocal enabledelayedexpansion
 
-:: TODO check maven instance
+set "DRY=0"
+FOR %%A IN (%*) DO (
+  IF /I "%%~A"=="--dry" SET "DRY=1"
+)
 
-ECHO !!! you need to manually set the JavaDoc incption version of all members
-ECHO !!! remember to set the corret version, e.g. convert to minor version, if code has patch version set
+:: TODO check maven instance
+:: FEATURE update JavaDoc inception versions
+:: FEATURE pin (Sonars) status badges values in README.md
+:: FEATURE check TODOs
+:: FEATURE update dependency version at README
+:: FEATURE mvn javadoc:javadoc
+
+ECHO HINT: you need to manually set the JavaDoc inception version of all members
+ECHO HINT: remember to set the corret version, e.g. convert to minor version, if code has patch version set
 PAUSE
 
+:: + + + + + check GIT status + + + + +
 git fetch origin || GOTO error
 
 FOR /f %%i IN ('git rev-parse --abbrev-ref HEAD') DO SET BRANCH=%%i
@@ -27,13 +40,35 @@ IF NOT "%LOCAL%"=="%REMOTE%" (
   GOTO error
 )
 
-:: FEATURE update JavaDoc inception versions
-:: FEATURE pin (Sonars) status badges values in README.md
-:: FEATURE check TODOs
-:: FEATURE update dependency version at README
-:: FEATURE mvn javadoc:javadoc
+git diff --quiet || (ECHO ERROR: unstaged changes & GOTO error)
+git diff --cached --quiet || (ECHO ERROR: staged but uncommitted changes & GOTO error)
+IF EXIST ".git\REBASE_HEAD" (ECHO ERROR: rebase in progress & GOTO error)
+IF EXIST ".git\MERGE_HEAD"  (ECHO ERROR: merge in progress & GOTO error)
+IF EXIST ".git\CHERRY_PICK_HEAD" (ECHO ERROR: cherry-pick in progress & GOTO error)
+:: - - - - - - check GIT status - - - - -
+:: + + + + + check GIT remote + + + + +
+FOR /f "delims=" %%u IN ('git remote get-url origin') DO SET "ORIGIN_URL=%%u"
+ECHO INFO: origin = %ORIGIN_URL%
 
-ECHO test | gpg --clearsign
+REM Soft warning if owner "voomdoon" isn't visible (supports SSH scp-style and https/ssh:// forms)
+SET "ORIGIN_OK="
+ECHO %ORIGIN_URL% | findstr /i ":voomdoon/" >nul && SET "ORIGIN_OK=1"
+IF NOT DEFINED ORIGIN_OK (
+  ECHO %ORIGIN_URL% | findstr /i "/voomdoon/" >nul && SET "ORIGIN_OK=1"
+)
+
+IF NOT DEFINED ORIGIN_OK (
+  ECHO WARNING: origin does not contain expected owner "voomdoon"
+  ECHO WARNING: continuing anyway (sync check vs origin/main will still protect you)
+)
+:: - - - - check GIT remote - - - - -
+
+ECHO test | gpg --clearsign >nul 2>&1 || (ECHO ERROR: GPG signing failed & GOTO error)
+
+IF "%DRY%"=="1" (
+  ECHO DRY RUN: stopping before release:prepare
+  GOTO end
+)
 
 ECHO release:prepare ...
 CALL mvn -Prelease -B release:prepare || GOTO maven_error
